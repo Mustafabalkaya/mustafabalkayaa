332c3ee0e8fba2add8735642c33822b935c589a6
Mustafabalkaya <balkayamustafa99@gmail.com>
2025-05-27 05:28:40 +0300

    son

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -14,11 +14,11 @@ function Projects() {
       <Particle />
       <Container>
         <h1 className="project-heading">
-          My Recent <strong className="purple">Works </strong>
+          Kişisel <strong className="purple">Çalışmalarım </strong>
         </h1>
         <p style={{ color: "white" }}>
-          Here are a few projects I've worked on recently.
-        </p>
+
+İşte son zamanlarda üzerinde çalıştığım birkaç proje.        </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
